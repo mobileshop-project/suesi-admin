aff3fd82ee91d793281ed2ecabeeb6685934d82e
PorPlaTakKom <apiwat.sri@icloud.com>
2021-11-06 14:35:00 +0700

    [update]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { DataGridPro } from '@mui/x-data-grid-pro';
+import { useDemoData } from '@mui/x-data-grid-generator';
 
 function App() {
+
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 100000,
+    editable: true,
+  });
+
+  const columns: GridColDef[] = [
+    { field: 'col1', headerName: 'Column 1', width: 150 },
+    { field: 'col2', headerName: 'Column 2', width: 150 },
+  ];
+
+  const rows: GridRowsProp = [
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +31,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="text-pink-500">Hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +41,19 @@ function App() {
           Learn React
         </a>
       </header>
+      <div style={{ height: 300, width: '100%' }}>
+        <DataGrid rows={rows} columns={columns} />
+      </div>
+      <div style={{ height: 520, width: '100%' }}>
+        <DataGridPro
+          {...data}
+          loading={data.rows.length === 0}
+          rowHeight={38}
+          checkboxSelection
+          disableSelectionOnClick
+        />
+      </div>
+
     </div>
   );
 }
